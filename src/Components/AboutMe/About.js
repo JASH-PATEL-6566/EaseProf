8debbdc12504e8ebc06ae00cfdd478f03dde70fb
Jash <100846378+JASH-PATEL-6566@users.noreply.github.com>
2022-06-03 02:19:20 +0530

    mail feature added

--- src/Components/AboutMe/About.js
+++ src/Components/AboutMe/About.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 export default function About() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_r303b9b', e.target, 'fLPGwHB1FRsrCxeCk')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    }
+
     return (
         <div className='about-container'>
-            <form>
+            <form onSubmit={sendEmail}>
                 <h3 className='about-name'>GET IN TOUCH</h3>
                 <div className='form-group-about'>
+                    <label htmlFor='subject'>Subject :</label>
+                    <input type="text" className='about-input' id="subject" name="subject" required />
+
                     <label htmlFor='user-name'>Fullname Name :</label>
                     <input type="text" className='about-input' id="user-name" name="user-name" required />
 
